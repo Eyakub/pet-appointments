a47413b103848bd8b1f127c106b96789307859c1
Eyakub Sorkar <eyakubsorkar@gmail.com>
2020-05-13 16:14:15 +0600

    add appointment works

--- src/components/App.js
+++ src/components/App.js
@@ -6,19 +6,36 @@ import AddAppointments from "./AddAppointments";
 import SearchAppointments from "./SearchAppointments";
 import ListAppointments from "./ListAppointments";
 
-import {without} from 'lodash';
+import { without } from "lodash";
 
 class App extends React.Component {
   state = {
     myName: "Eyakub",
     myAppointments: [],
-    lastIndex: 0
+    formDisplay: false,
+    lastIndex: 0,
   };
 
-  deleteAppointment = (apt) =>{
+  deleteAppointment = (apt) => {
     let temApts = this.state.myAppointments;
     temApts = without(temApts, apt);
-    this.setState({myAppointments: temApts})
+    this.setState({ myAppointments: temApts });
+  };
+
+  toggleForm = () => {
+    this.setState({
+      formDisplay: !this.state.formDisplay
+    });
+  };
+
+  addAppointment = (apt) => {
+    let tempApts = this.state.myAppointments;
+    apt.aptId = this.state.lastIndex;
+    tempApts.unshift(apt); // push at the beginning using unshift
+    this.setState({
+      myAppointments: tempApts,
+      lastIndex: this.state.lastIndex + 1
+    })
   }
 
   componentDidMount() {
@@ -27,7 +44,7 @@ class App extends React.Component {
       .then((result) => {
         const apts = result.map((item) => {
           item.aptId = this.state.lastIndex;
-          this.setState({lastIndex: this.state.lastIndex + 1})
+          this.setState({ lastIndex: this.state.lastIndex + 1 });
           return item;
         });
         this.setState({
@@ -42,9 +59,16 @@ class App extends React.Component {
           <div className="row">
             <div className="col-md-12 bg-white">
               <div className="container">
-                <AddAppointments />
+                <AddAppointments
+                  formDisplay={this.state.formDisplay}
+                  toggleForm={this.toggleForm}
+                  addAppointment={this.addAppointment}
+                />
                 <SearchAppointments />
-                <ListAppointments appointments={this.state.myAppointments} deleteAppointment={this.deleteAppointment} />
+                <ListAppointments
+                  appointments={this.state.myAppointments}
+                  deleteAppointment={this.deleteAppointment}
+                />
               </div>
             </div>
           </div>
